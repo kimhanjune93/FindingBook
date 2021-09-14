3a786261adf6d3b0f1b178a81946ba1939e54373
kimhanjune93 <kimhanjune93@gmail.com>
2021-09-14 23:55:00 +0900

    login popup

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ def home():
 
         return render_template('index.html')
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        return redirect(url_for("/", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return redirect(url_for("/", msg="로그인 정보가 존재하지 않습니다."))
 
 
 @app.route('/login')
